f012bd16e2e36a2ff017d68dc9553e21b89253f6
Student <b3028830@my.shu.ac.uk>
2016-02-09 12:52:07 +0000

    Tested the class Course, Implemented scaffolding and bootstrapping

---
+++ StudentEnrol/Course.groovy
@@ -0,0 +1,31 @@
+package com.studentenrol
+
+class Course {
+
+String department
+
+String title
+
+String leader
+
+String code
+
+Date startDate
+
+Date endDate
+
+String description
+
+String toString(){
+
+"$title,$code,$department"
+
+}
+
+static constraints={
+
+
+
+}
+
+}
